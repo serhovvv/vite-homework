51a08465536f5c668e5de8347d529abef6eeeb96
serhovvv <oleksii.serh@gmail.com>
2025-03-21 12:30:27 +0200

    tech: feedback section adaptive

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,11 +11,19 @@ window.addEventListener('scroll', () => {
 
 
 const swiper = new Swiper(' .feedback .swiper', {
-  slidesPerView: 2,
+  slidesPerView: 1,
+  centeredSlides: true,
   navigation: {
     nextEl: '.slider-btn--next',
     prevEl: '.slider-btn--prev',
   },
+  breakpoints: {
+    // when window width is >= 320px
+    992: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    }
+  }
 });
 
 // tabs
